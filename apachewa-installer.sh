dfe8e29c898549d28766f2b9e7aab3aa89570ccf
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2015-02-25 23:25:15 -0500

    added docker-compose.yml to use docker-compose (fig) for demo setup and run. added endpoint check to apachewa installer to wait for openam server initialization before installing web agent on apache server

--- apachewa-installer.sh
+++ apachewa-installer.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+wait_for_endpoint() {
+	until $(curl --output /dev/null --silent --head --insecure --fail $1); do
+		printf "."
+		sleep 5s
+	done
+	echo "done!"
+}
+
+echo "Waiting for https://openam.example.com:8443/openam to finish startup"
+wait_for_endpoint https://openam.example.com:8443/openam
+echo "Endpoint up, continuing..."
 
 echo "Creating /etc/apache2/httpd.conf (workaround for Debian/Ubuntu Apache2 modifications)"
 touch /etc/apache2/httpd.conf
